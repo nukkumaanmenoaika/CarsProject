4e484932982ed1f4abfe61a3ea02a870d1391c0b
nukkumaanmenoaika <misha_bro@list.ru>
2024-06-07 11:56:01 +0900

    v1.1

--- PathLists/PathLists/AddWindows/AddCars.xaml.cs
+++ PathLists/PathLists/AddWindows/AddCars.xaml.cs
@@ -33,33 +33,60 @@ namespace PathLists.AddWindows
 
         private void Button_Add_Click(object sender, RoutedEventArgs e) // Обработчик событий для кнопки, предназначенный для валидации, сохранения данных и перехода в окно просмотра автомобилей
         {
-            string[] license = TextBoxLicenseNumber.Text.Split(' ');
-            string brand = TextBoxBrand.Text.Trim(), year = TextBoxYear.Text.Trim();
-            if (license.Length != 4)
+            try
             {
-                TextBoxLicenseNumber.ToolTip = CommonVariables.errorField;
-                TextBoxLicenseNumber.Background = CommonVariables.errorColor;
+                string[] license = TextBoxLicenseNumber.Text.Split(' ');
+                string brand = TextBoxBrand.Text.Trim();
+                bool nonumisDigit = false;
+                bool nonumisDigitlast = false;
+                string year = TextBoxYear.Text.Trim();
+                if (license.Length == 4)
+                {
+                    foreach (char a in license[1])
+                    {
+                        if (!Char.IsDigit(a)) nonumisDigit = true;
+                    }
+                    foreach (char b in license[3])
+                    {
+                        if (!Char.IsDigit(b)) nonumisDigitlast = true;
+                    }
+                }
+                else
+                {
+                    TextBoxLicenseNumber.ToolTip = CommonVariables.errorField;
+                    TextBoxLicenseNumber.Background = CommonVariables.errorColor;
+                }
+                if (license.Length != 4 || license[0].Length != 1 || license[1].Length != 3 || license[2].Length != 2 || license[3].Length != 2 || nonumisDigit || nonumisDigitlast)
+                {
+                    TextBoxLicenseNumber.ToolTip = CommonVariables.errorField;
+                    TextBoxLicenseNumber.Background = CommonVariables.errorColor;
+                }
+                else if (brand.Length < 5)
+                {
+                    TextBoxBrand.ToolTip = CommonVariables.errorField;
+                    TextBoxBrand.Background = CommonVariables.errorColor;
+                }
+                else if (Convert.ToInt32(year) > 2005 || Convert.ToInt32(year) < 1980)
+                {
+                    TextBoxYear.ToolTip = CommonVariables.errorField;
+                    TextBoxYear.Background = CommonVariables.errorColor;
+                }
+                else
+                {
+                    TextBoxLicenseNumber.ToolTip = CommonVariables.fixedField;
+                    TextBoxLicenseNumber.Background = CommonVariables.fixedColor;
+                    TextBoxBrand.ToolTip = CommonVariables.fixedField;
+                    TextBoxBrand.Background = CommonVariables.fixedColor;
+                    TextBoxYear.ToolTip = CommonVariables.fixedField;
+                    TextBoxYear.Background = CommonVariables.fixedColor;
+                    addCar(String.Join("", license), brand, year);
+                }
             }
-            else if (brand.Length < 5)
+            catch
             {
-                TextBoxBrand.ToolTip = CommonVariables.errorField;
-                TextBoxBrand.Background = CommonVariables.errorColor;
+                MessageBox.Show("Неверный формат данныых");
             }
-            else if (year.Length > 2005 & year.Length < 1980)
-            {
-                TextBoxYear.ToolTip = CommonVariables.errorField;
-                TextBoxYear.Background = CommonVariables.errorColor;
-            }
-            else
-            {
-                TextBoxLicenseNumber.ToolTip = CommonVariables.fixedField;
-                TextBoxLicenseNumber.Background = CommonVariables.fixedColor;
-                TextBoxBrand.ToolTip = CommonVariables.fixedField;
-                TextBoxBrand.Background = CommonVariables.fixedColor;
-                TextBoxYear.ToolTip = CommonVariables.fixedField;
-                TextBoxYear.Background = CommonVariables.fixedColor;
-                addCar(String.Join("", license), brand, year);
-            }
+                
         }
 
         private void addCar(string license, string brand, string year) // Метод добавления автомобилей в базу данных
